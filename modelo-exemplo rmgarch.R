9f7f19a7f1e996a26c31e2e5944e77dfa9aa2b81
rfbressan <rfbressan@gmail.com>
2017-08-10 17:46:08 -0300

    Simulacao rmgarch. Copula t com margens GPD.

--- modelo-exemplo rmgarch.R
+++ modelo-exemplo rmgarch.R
@@ -94,9 +94,9 @@ if(fit_tc@mfit$convergence != 0) stop("cGARCHfit nao convergiu")
 
 # Rolling Simulation ------------------------------------------------------
 t_orig <- nrow(ret) - n_roll
-simMu <- simS <- filtMu <- filtS <- matrix(NA, ncol = d, nrow = n_roll)
-simC <- filtC <- array(NA, dim = c(d, d, n_roll))
-colSd <- function(x) apply(x, 2, "sd")
+# simMu <- simS <- filtMu <- filtS <- matrix(NA, ncol = d, nrow = n_roll)
+# simC <- filtC <- array(NA, dim = c(d, d, n_roll))
+# colSd <- function(x) apply(x, 2, "sd")
 specx <- t_cspec
 
 for(i in 1:d) specx@umodel$fixed.pars[[i]] <-  as.list(fit_tc@model$mpars[fit_tc@model$midx[,i]==1,i])
@@ -106,49 +106,44 @@ setfixed(specx) <- as.list(fit_tc@model$mpars[fit_tc@model$midx[,d+1]==1, d+1])
 filtro_c <- cgarchfilter(specx, ret, 
                        filter.control = list(n.old = t_orig),
                        spd.control = list(lower = 0.1, upper = 0.9, type = "mle", kernel = "normal"))
+# Matrizes de condicoes iniciais
+# Devem conter o numero de observacoes a serem simuladas + a quantidade necessaria para as CIs
+presigmas <- matrix(tail(sigma(filtro_c), n_roll+max(c(m, s, p, q))), ncol = d)
+preresiduals <- matrix(tail(residuals(filtro_c), n_roll+max(c(m, s, p, q))), ncol = d)
+prereturns <- matrix(tail(coredata(ret), n_roll+max(c(m, s, p, q))), ncol = d) # Retornos observados
 
-presigmas <- matrix(tail(sigma(filtro_c), n_roll+s), ncol = d)
-preresiduals <- matrix(tail(residuals(filtro_c), n_roll+s), ncol = d)
-prereturns <- matrix(tail(coredata(ret), n_roll+s), ncol = d) # Retornos observados
+# pre-aloca os arrays com as simulacoes 1 passo a frente e n_ahead passos a frente
+simx1 <- array(dim = c(m_sim, d, n_roll))
+simx_ahead <- array(dim = c(m_sim, d, n_roll))
 
 for(i in 1:n_roll){
-  # if(i==1){
-  #   presigma = matrix(tail(sigma(fit_tc), s), ncol = d)
-  #   # arma = c(p,q) therefore need p lags
-  #   prereturns = matrix(unlist(Dat[(t_orig-p):t_orig, ]), ncol = d, nrow = p)
-  #   preresiduals = matrix(tail(residuals(fit_tc),2), ncol = d, nrow = max(c(q,m)))
-  #   
-  #   tmp = cgarchfilter(specx, Dat[1:(t_orig+1), ], filter.control = list(n.old = t_orig))
-  #   filtMu[i,] = tail(fitted(tmp), 1)
-  #   filtS[i,] = tail(sigma(tmp), 1)
-  #   filtC[,,i] = last(rcov(tmp))[,,1]
-  # } else{
-  #   presigma = matrix(tail(sigma(tmp), 2), ncol = 3)
-  #   # arma = c(2,1) therefore need 2 lags
-  #   prereturns = matrix(unlist(Dat[(t_orig+i-2):(t_orig+i-1), ]), ncol = 3, nrow = 2)
-  #   preresiduals = matrix(tail(residuals(tmp),2), ncol = 3, nrow = 2)
-  #   
-  #   tmp = cgarchfilter(specx, Dat[1:(t_orig+i), ], filter.control = list(n.old = t_orig))			
-  #   filtMu[i,] = tail(fitted(tmp), 1)
-  #   filtS[i,] = tail(sigma(tmp), 1)
-  #   filtC[,,i] = last(rcov(tmp))[,,1]
-  # }
-  sim3 = cgarchsim(fit_tc, n.sim = 1, m.sim = 10000, startMethod = "sample", prereturns = prereturns,
-                   presigma = presigma, preresiduals = preresiduals)
-  simx = t(sapply(sim3@msim$simX, FUN = function(x) x[1,]))
-  simMu[i,] = colMeans(simx)
-  simS[i,] = colSd(simx)
-  simC[,,i] = cov(simx)
-  print(i)
-  
+  sim_roll <-  cgarchsim(fit_tc, n.sim = n_ahead, m.sim = m_sim, startMethod = "sample",
+                   prereturns = prereturns[i:(max(c(m, s, p, q))+i-1), ,drop = FALSE],
+                   presigma = presigmas[i:(max(c(m, s, p, q))+i-1), ,drop = FALSE], 
+                   preresiduals = preresiduals[i:(max(c(m, s, p, q))+i-1), ,drop = FALSE])
+  simx1[,,i] <-  t(sapply(sim_roll@msim$simX, FUN = function(x) x[1,]))
+  # simx_ahead deve ser o retorno CUMULATIVO do dia 1 ate n_ahead.
+  simx_ahead[,,i] <-  t(sapply(sim_roll@msim$simX, FUN = function(x) apply(x, 2, sum)))
   # CHECK:
   # X[t+1] = mu[t+1] + e[t+1]
   # sim3@msim$simX[[i]][1,] - (sim3@msim$simZ[,,i]*sqrt(diag(sim3@msim$simH[[i]][,,1])))
   # is equal to filtMu[i,]
-  if(i < 3 ){
-    print(all.equal(sim3@msim$simX[[2]][1,] - (sim3@msim$simZ[,,2]*sqrt(diag(sim3@msim$simH[[2]][,,1]))), 
-                    filtMu[i,]))
-    print(all.equal(sim3@msim$simX[[10000]][1,] - (sim3@msim$simZ[,,10000]*sqrt(diag(sim3@msim$simH[[10000]][,,1]))), 
-                    filtMu[i,]))
-  }
+  # if(i < 3 ){
+  #   print(all.equal(sim3@msim$simX[[2]][1,] - (sim3@msim$simZ[,,2]*sqrt(diag(sim3@msim$simH[[2]][,,1]))), 
+  #                   filtMu[i,]))
+  #   print(all.equal(sim3@msim$simX[[10000]][1,] - (sim3@msim$simZ[,,10000]*sqrt(diag(sim3@msim$simH[[10000]][,,1]))), 
+  #                   filtMu[i,]))
+  
 }
+# Arrays simx possuem as seguintes dimensoes [m_sim, d, n_roll]
+# Grafico com os caminhos das simulacoes
+plot(1:n_roll, simx1[1,1,], type = "l", ylim = c(min(simx1[,1,]), max(simx1[,1,])), col = "blue")
+for(i in 2:m_sim) lines(1:n_roll, simx1[i,1,], col = i)
+lines(1:n_roll, coredata(ret)[(t_orig+1):nrow(ret), 1], col = "black")
+
+
+# Composicao do portfolio -------------------------------------------------
+# array de dimensao [m_sim, n_roll] com os retornos simulados de 1 passo a frente
+port_simx1 <- apply(simx1, c(1,3), `%*%`, wport)
+port_simx_ahead <- apply(simx_ahead, c(1,3), `%*%`, wport)
+
